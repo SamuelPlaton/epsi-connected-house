a93a4d9473fd281bd3d1a04ff4e6afab1064fe24
Samuel Platon <samuel@binch-solutions.com>
2021-05-28 10:11:48 +0200

    feat: add chart component

---
+++ app/src/components/historic-chart/HistoricChart.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="w-1/2 h-3/4 mx-auto">
+    <line-chart :chart-data="datas" />
+  </div>
+</template>
+
+<script>
+import LineChart from './local-components/LineChart.js' ;
+import moment from 'moment';
+
+export default {
+  components: { LineChart },
+  name: "HistoricChart",
+  props: {
+    historicArray: Array,
+    label: String,
+  },
+  data () {
+    return {
+      datas: { labels: [], datasets: []}
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData() {
+      moment.locale('fr');
+      const filteredHistoricArray = this.historicArray.filter((el) => el.value !== null);
+      const historicDates = filteredHistoricArray.map((el) => {
+        const date = moment(el.date);
+        return date.format('L LT');
+      });
+
+      this.datas = {
+        labels: historicDates,
+        datasets: [{
+          label: this.label,
+          backgroundColor: "transparent",
+          borderColor: "rgba(1, 116, 188, 0.50)",
+          pointBackgroundColor: "rgba(171, 71, 188, 1)",
+          data: filteredHistoricArray.map((el) => el.value),
+        }]
+      }
+    },
+  }
+}
+</script>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -3,20 +3,28 @@
     <p class="my-2 text-lg font-semibold">
       Welcome to the front app
     </p>
-    <pre>{{this.houses}}</pre>
+    <pre>{{ houses }}</pre>
+    <HistoricChart :historic-array="historic" label='Température' v-if="historic.length > 0"/>
   </div>
 </template>
 
 <script>
 import {Api} from '@/api';
+import { HistoricChart } from "@/components";
 
 export default {
   name: "Home",
+  components: {HistoricChart},
   data() {
-    houses: []
+    return {
+      houses: [],
+      historic: [],
+    }
   },
   async beforeCreate() {
     const houses = await Api.HousesApi.list();
+    const detector = await Api.DetectorsApi.get(1, 'thermo');
+    this.historic = detector.historic,
     this.houses = houses;
   }
 };
